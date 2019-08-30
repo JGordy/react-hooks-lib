1d705f898cf0eec92d77aa78e253de1208b42baa
Joseph Gordy <jgordy2424@gmail.com>
2019-08-30 19:11:16 -0400

    Add useLocalStorage, useMedia, and useDarkMode hooks

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import * as React from 'react'
-
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
-
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
-    }
-  }, [])
-
-  return counter
-}
+export { default as useDarkMode } from './useDarkMode';
+export { default as useHover } from './useHover';
+export { default as useLocalStorage } from './useLocalStorage';
+export { default as useMedia } from './useMedia';
+export { default as useScript } from './useScript';
+export { default as useWhyDidYouUpdate } from './useWhyDidYouUpdate';
