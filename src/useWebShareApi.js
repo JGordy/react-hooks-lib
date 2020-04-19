3e34a33f57ddf25b444c6983cf73242ebb909af1
Joseph Gordy <jgordy2424@gmail.com>
2020-04-19 17:31:52 -0400

    Fix typo/Add test

--- src/useWebShareApi.js
+++ src/useWebShareApi.js
@@ -7,20 +7,20 @@ const useWebShareApi = ({
 }) => {
 	const isSupported = !!navigator.share;
 
+	// TODO: Change to async/await
 	const share = isSupported && navigator.share({
 		title,
 		text,
 		url,
-	}).then((resposne) => {
+	}).then((response) => {
 		if (onShareSuccess) {
-			onShareSuccess(resposne);
+			onShareSuccess(response);
 		}
-	})
-		.catch(err => {
-			if (onShareError) {
-				onShareError(err);
-			}
-		});
+	}).catch(err => {
+		if (onShareError) {
+			onShareError(err);
+		}
+	});
 
 	return [isSupported, share];
 }
